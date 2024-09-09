5ad0ff24c42e7f69f31f0ed132dadeac69592a4b
Evgeny Ilyin <evgeniy.v.ilyin@gmail.com>
2024-09-09 09:53:18 +0300

    webp quality 90

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -17,7 +17,7 @@ export const img = () => {
 
 			// создать webp
 			.pipe(app.plugins.newer(app.path.build.img))
-			.pipe(webp({ quality: 80 }))
+			.pipe(webp({ quality: 90 }))
 			.pipe(app.gulp.dest(app.path.build.img))
 
 			// перенести все картинки без оптимизации (dev), чтобы работал лайтбокс Fancybox по ссылке на картинку (без замены на picture и webp)
